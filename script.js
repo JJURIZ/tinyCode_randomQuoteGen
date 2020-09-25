6d92266844ee116220864cc6ccaf82446c19b617
jjuriz <jjuriz@gmail.com>
2020-09-25 17:43:54 -0400

    API's for Twitter and Quote Gen site plugged in

--- script.js
+++ script.js
@@ -1,14 +1,67 @@
 "use strict"
 
-// GET QUOTE
-async function getQuote() {
-    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
-    try {
+const quoteContainer = document.getElementById('quote__container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
-    } catch(error) {
-        console.log("The quote failed", error)
+// SHOW LOADING
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// HIDE LOADING
+function complete() {
+    if (!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
     }
 }
+
+// GET QUOTE
+async function getQuote() {
+    loading();
+    const proxyUrl = 'https://sheltered-shelf-27449.herokuapp.com/';
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try {
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+        // Ensures either author name or Unknown appears rather than a blank author
+        if (data.quoteAuthor === '') {
+            authorText.innerText = "-Unknown-"
+        } else {
+            authorText.innerText = data.quoteAuthor;
+        }
+
+        //Reduces the font size for quotes longer than 50 characters
+         if (data.quoteText.length > 120) {
+            quoteText.classList.add('long-quote');
+         } else {
+             quoteText.classList.remove('long-quote');
+         }
+        quoteText.innerText = data.quoteText;
+
+        complete();
+    } catch (error) {
+        getQuote();
+    }
+}
+
+// TWEET QUOTE
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`
+    window.open(twitterUrl, '_blank');
+};
+
+// EVENT LISTENERS
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
 
 
 //ON LOAD
